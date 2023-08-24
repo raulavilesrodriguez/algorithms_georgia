f25ae35b142e6ee24fb61b6c4223db0f6cd05654
raulavilesrodriguez <finbravilesr@gmail.com>
2023-08-24 16:58:19 -0500

    modified tree code

--- 4. Tree Traversals/BST.java
+++ 4. Tree Traversals/BST.java
@@ -1,6 +1,7 @@
 public class BST<T extends Comparable<? super T>> {
     private BSTNode<T> root;
     private int size;
+    private BSTNode<T> succesor;
 
     /*Constructor methods*/
     public BST(){
@@ -42,7 +43,76 @@ public class BST<T extends Comparable<? super T>> {
         if(data == null){
             throw new IllegalArgumentException("Error: You cannot add null data to the tree");
         }
-        
+        if(curr == null){
+            return null;
+        }
+        else if(data.compareTo(curr.getData())<0){
+            curr.setLeft(removeH(data, curr.getLeft()));
+        }
+        else if(data.compareTo(curr.getData())>0){
+            curr.setRight(removeH(data, curr.getRight()));
+        }
+        else{
+            size--;
+            if(curr.getLeft() == null && curr.getRight() == null){
+                return null;
+            }
+            else if(curr.getLeft() == null){
+                return curr.getRight();
+            }
+            else if(curr.getRight() == null){
+                return curr.getLeft();
+            }
+            else{
+                curr.setRight(removeSuccesor(curr.getRight()));
+                curr.setData(succesor.getData());
+            }
+        }
+        return curr;
     }
+
+    private BSTNode<T> removeSuccesor(BSTNode<T> curr){
+        if(curr.getLeft() == null){
+            succesor = curr;
+            return curr.getRight();
+        }
+        else{
+            curr.setLeft(removeSuccesor(curr.getLeft()));
+            return curr;
+        }
+    }
+
+    public T find(T data){
+        return findH(data, root);
+    }
+
+    private T findH(T data, BSTNode<T> curr){
+        if(curr == null){
+            return null;
+        }
+        else if(data.compareTo(curr.getData())<0){
+            return findH(data, curr.getLeft());
+        }
+        else if(data.compareTo(curr.getData())>0){
+            return findH(data, curr.getRight());
+        }
+        else {
+            return curr.getData();
+        }
+    }
+
+    public void preOrder(BSTNode<T> root){
+        preOrderH(root);
+    }
+
+    private void preOrderH(BSTNode<T> curr){
+        if(curr != null){
+            System.out.print(curr.getData());
+            preOrderH(curr.getLeft());
+            preOrderH(curr.getRight());
+        }
+    }
+
+    
 
 }
---
+++ 4. Tree Traversals/BSTest.java
@@ -0,0 +1,6 @@
+public class BSTest{
+    public static void main(String[] args){
+        BST<Integer> tree = new BST<>();
+        
+    }
+}
